50981935f87902deed1deb9cf9cbb60ae9824150
lmq <2965892623@qq.com>
2021-08-10 19:41:00 +0800

    hot fix commit 1

--- src/main/java/com/lmq/gittest/hello.java
+++ src/main/java/com/lmq/gittest/hello.java
@@ -3,5 +3,6 @@ package com.lmq.gittest;
 public class hello {
     public static void main(String[] args) {
         System.out.println("third commit");
+        System.out.println("hot fix1");
     }
 }
