604d8f2a57dbc80a70f2ce7a9b3d3a10aafb4796
lmq <2965892623@qq.com>
2021-08-10 19:16:04 +0800

    second commit

--- src/main/java/com/lmq/gittest/hello.java
+++ src/main/java/com/lmq/gittest/hello.java
@@ -2,6 +2,6 @@ package com.lmq.gittest;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.println("first commit");
+        System.out.println("second commit");
     }
 }
